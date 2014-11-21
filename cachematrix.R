f535b0d58c9e49466a9ae3833611bd8e4f80a04f
John Williams <jwilliams@integralsoft.com>
2014-11-21 14:04:16 -0500

    cachematrix code submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## -------------
+## By: John Williams 11/20/14
+## Purpose: 
+##      2 functions demonstrate caching of data, which can vastly improve performance when
+##          performing operations on large data sets. In this example, the inverse of a
+##          matrix is cached.
+## 
 
-## Write a short comment describing this function
+## Function: makeCacheMatrix
+## -------------------------
+## Purpose: get and set matrix and its inverse
+## Parameter: square matrix data
+## Assumptions: supplied matrix is invertible
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+    xInverse <- NULL
+    set <- function(y) {
+        x <<- y
+        xInverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) xInverse <<- solve
+    getInverse <- function() xInverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
+## Function: cacheSolve
+## --------------------
+## Purpose: Process a passed function containing a matrix and the inverse
+##  If the inverse data is cached, return the cached data;
+##      otherwise call the solve() function on the matrix, cache and return the data
+## Parameter: a makeCacheMatrix function
 
-## Write a short comment describing this function
+#The next line is my sample invertible matrix
+# y <- matrix(c(1,0,5,2,1,6,3,4,0), nrow=3, ncol=3)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    xInverse <- x$getInverse()
+    if(!is.null(xInverse)) {
+        message("getting cached data")
+        return(xInverse)
+    }
+    data <- x$get()
+    xInverse <- solve(data, ...)
+    x$setInverse(xInverse)
+    xInverse
 }
